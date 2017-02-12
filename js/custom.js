c678b9b3a476891b62f86278e946006884ed860f
Krzysztof Sokalla <k.sokalla@gmail.com>
2017-02-12 16:57:56 +0100

    animacja finito

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,21 @@
 'use strict';
-
-
+$(document).ready(function () {
+	$("#kwadrat").css({
+		"background-color": "red"
+		, "width": "200px"
+		, "height": "200px"
+	});
+	$("#klik").click(function () {
+		$("#kwadrat").animate({
+			"margin-left": "100px"
+			, "width": "100px"
+			, "height": "100px"
+		}, 3000, function () {
+			$("#kwadrat").animate({
+				"background-color": "blue"
+			}, 5000, function () {
+				$("#kwadrat").append("<h2>Gratulacje, animacja zakończona</h2>");
+			});
+		});
+	});
+});
